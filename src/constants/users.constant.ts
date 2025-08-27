5ac416048cc26eed51d0b3b8722435eadc9d7567
sebastianthomas94 <53168297+sebastianthomas94@users.noreply.github.com>
2025-08-27 09:51:40 +0530

    feat: added leaders only for random picking

---
+++ src/constants/users.constant.ts
@@ -0,0 +1,42 @@
+export const USERS = [
+  {
+    id: "U07TF4BDWVC",
+    name: "Sebastian M Thomas",
+  },
+  {
+    id: "U07TJSVQMSR",
+    name: "ejas",
+  },
+  {
+    id: "U07TMNM8GJW",
+    name: "Alan Varghese",
+  },
+  {
+    id: "U07U0DB8WAV",
+    name: "arshad",
+  },
+  {
+    id: "U07U0DBAF09",
+    name: "Mohammed Jahas",
+  },
+  {
+    id: "U07U0DBDDHP",
+    name: "Asharudeen Ali",
+  },
+  {
+    id: "U0811TDLKED",
+    name: "Amritha S",
+  },
+  {
+    id: "U083X7BGV2A",
+    name: "Ashwin KV",
+  },
+  {
+    id: "U084GGHC4GH",
+    name: "Sayed Adinan",
+  },
+  {
+    id: "U08UE2QHV60",
+    name: "Jancy Daniel",
+  },
+];
